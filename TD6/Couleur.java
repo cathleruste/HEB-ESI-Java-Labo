00b88071df4c8a581cd5c6aeb5e6c79086cfbf99
clr <cleruste@heb.be>
2012-05-19 08:22:43 +0100

    Integration des rmq

---
+++ TD6/Couleur.java
@@ -0,0 +1,65 @@
+package java.tds.td6.classes;
+
+/**
+ * regroupe les codes de différentes couleurs 
+ */
+public class Couleur{
+	
+	/** retourne le code couleur du noir SOUS LINUX.
+	 * @return code couleur du noir
+	 */
+	public static String getNoir(){
+		return "\033[30m";
+	}
+
+	/** retourne le code couleur du blanc SOUS LINUX.
+	 * @return code couleur du blanc
+	 */
+	public static String getBlanc(){
+		return "\033[37m";
+	}
+
+	/** retourne le code couleur du rouge SOUS LINUX.
+	 * @return code couleur du rouge
+	 */
+	public static String getRouge(){
+		return "\033[31m";
+	}
+
+	/** retourne le code couleur du jaune SOUS LINUX.
+	 * @return code couleur du jaune
+	 */
+	public static String getJaune(){
+		return "\033[33m";
+	}
+
+	/** retourne le code couleur du vert SOUS LINUX.
+	 * @return code couleur du vert
+	 */
+	public static String getVert(){
+		return "\033[32m";
+	}
+
+	/** retourne le code couleur du bleu SOUS LINUX.
+	 * @return code couleur du bleu
+	 */
+	public static String getBleu(){
+		return "\033[34m";
+	}
+
+	/** retourne le code couleur du rose SOUS LINUX.
+	 * @return code couleur du rose
+	 */
+	public static String getRose(){
+		return "\033[35m";
+	}
+
+	/** retourne le code couleur du gris SOUS LINUX.
+	 * @return code couleur du gris
+	 */
+	public static String getGris(){
+		return "\033[38m";
+	}
+}
+
+
